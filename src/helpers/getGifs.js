d16ad7dabf88e955bff8f36d78d7acb3b4c19cb1
Yolvys Ferrer <yolvysferrer@gmail.com>
2024-04-16 16:32:06 -0400

    corregido el protocolo en el fetch

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getImages = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=RXch6Ibs8Pn6W1pErdYe8Xa83MUQkDdQ&limit=10&q=${category}`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=RXch6Ibs8Pn6W1pErdYe8Xa83MUQkDdQ&limit=10&q=${category}`;
     const resp = await fetch(url);
     const {data} = await resp.json();
     const gifs = data.map(gif =>({
